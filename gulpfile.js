25f215f9697978c19fb24e5f339b875800058709
周培公 <peigong@foxmail.com>
2015-08-07 10:06:27 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
-var copy = require('gulp-copy');
+var clean = require('gulp-clean');
 var jade = require('gulp-jade');
 
 gulp.task('default', function() {
-    gulp.src('./src/favicon.ico')
-    .pipe(copy('./dist/jesse/'));
+    gulp.src('./dist')
+    .pipe(clean({ force: true }));
+
+    gulp.src('./src/favicon.ico', { base: './src' })
+    .pipe(gulp.dest('./dist/jesse/'));
 
     gulp.src('./src/*.jade')
     .pipe(jade())
