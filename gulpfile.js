fe9e041f1c3707cde8744a8f2cafb89994c27836
周培公 <peigong@foxmail.com>
2015-08-11 10:10:44 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var jade = require('gulp-jade');
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
+gulp.task('clean', function() {
     gulp.src('./dist')
-    .pipe(clean({ force: true }));
+    .pipe(plugins.clean({ force: true }));
+});
 
+gulp.task('move', ['clean'], function() {
     gulp.src('./src/favicon.ico', { base: './src' })
     .pipe(gulp.dest('./dist/jesse/'));
+});
 
+gulp.task('less', ['clean'], function() {
+    gulp.src('./src/less/*.less')
+    .pipe(plugins.less())
+    .pipe(gulp.dest('./dist/jesse/styles'));
+});
+
+gulp.task('jade', ['less'], function() {
     gulp.src('./src/*.jade')
-    .pipe(jade())
+    .pipe(plugins.jade())
     .pipe(gulp.dest('./dist/jesse/'));
 });
+
+gulp.task('default', ['move', 'jade']);
