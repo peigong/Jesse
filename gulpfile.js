1797a57c49823d6cfef132820772d3d070ddb99f
周培公 <peigong@foxmail.com>
2015-08-13 09:15:02 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,64 @@
+var del = require('del');
 var gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')();
+    $ = require('gulp-load-plugins')();
 
-gulp.task('clean', function() {
-    gulp.src('./dist')
-    .pipe(plugins.clean({ force: true }));
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var config = {
+    path: {
+      less: './src/less/*.less',
+      jade: './src/*.jade'
+    },
+    dist: {
+      css: './dist/jesse/styles',
+      jade: './dist/jesse'
+    }
+};
+gulp.task('archive', function(cb) {
+  runSequence([
+      'archive:copy:favicon',
+      'archive:copy:css'
+    ], cb);
 });
 
-gulp.task('move', ['clean'], function() {
-    gulp.src('./src/favicon.ico', { base: './src' })
+gulp.task('archive:copy:favicon', function() {
+    return gulp.src('./src/favicon.ico', { base: './src' })
     .pipe(gulp.dest('./dist/jesse/'));
 });
 
-gulp.task('less', ['clean'], function() {
-    gulp.src('./src/less/*.less')
-    .pipe(plugins.less())
-    .pipe(gulp.dest('./dist/jesse/styles'));
+gulp.task('archive:copy:css', function() {
+    return gulp.src('./src/css/*.css', { base: './src/css' })
+    .pipe(gulp.dest(config.dist.css))
 });
 
-gulp.task('jade', ['less'], function() {
-    gulp.src('./src/*.jade')
-    .pipe(plugins.jade())
-    .pipe(gulp.dest('./dist/jesse/'));
+gulp.task('build:clean', function(cb) {
+    del(['./dist', './.tmp'], cb);
 });
 
-gulp.task('default', ['move', 'jade']);
+gulp.task('build:less', function() {
+    gulp.src(config.path.less)
+    .pipe($.less())
+    .pipe(gulp.dest(config.dist.css))
+});
+
+gulp.task('build:jade', function() {
+    gulp.src(config.path.jade)
+    .pipe($.jade())
+    .pipe(gulp.dest(config.dist.jade))
+});
+
+gulp.task('build', function(cb) {
+    runSequence(
+        'build:clean',
+        ['archive', 'build:less', 'build:jade'],
+    cb);
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.path.less, ['build:less']);
+    gulp.watch(config.path.jade, ['build:jade']);
+});
+
+gulp.task('default', ['build', 'watch']);
